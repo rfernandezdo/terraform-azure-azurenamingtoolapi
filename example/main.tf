cafdc64af0d85cd7039546a6fe1e9335705894d2
Rafael Fernández <rafael_fernandez@outlook.com>
2025-06-02 20:18:49 +0200

    refactor(main.tf): remove resource_type variable and add resource group definition

--- example/main.tf
+++ example/main.tf
@@ -6,11 +6,20 @@ module "azurenamingtool" {
   tenant_id     = var.tenant_id
   client_secret = var.client_secret
   api_app_id    = var.api_app_id
-  resource_type = var.resource_type
   components    = var.components
 }
 
 output "generated_name" {
   value = module.azurenamingtool.generated_name
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = module.azurenamingtool.generated_name
+  location = "West Europe"
+  tags = {
+    environment = "example"
+    project     = "azurenamingtool"
+  }
+  depends_on = [module.azurenamingtool.show_name_generation]
 }
 
