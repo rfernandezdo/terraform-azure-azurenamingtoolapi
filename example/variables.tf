5a342052c16bb1c13a3d0ab5cc02ad4b10c6b71c
Rafael Fernández <rafael_fernandez@outlook.com>
2025-06-02 20:16:25 +0200

    refactor(variables): enhance variable descriptions and remove unused variables

--- example/variables.tf
+++ example/variables.tf
@@ -1,17 +1,36 @@
-variable "api_key" {}
+variable "api_key" {
+  description = "API key for Azure Naming Tool API."
+  type        = string
+}
 
-variable "client_id" {}
+variable "client_id" {
+  description = "Azure AD App Registration (client) - client_id"
+  type        = string
+}
 
-variable "tenant_id" {}
+variable "tenant_id" {
+  description = "Azure AD tenant_id"
+  type        = string
+}
 
-variable "client_secret" {}
+variable "client_secret" {
+  description = "Azure AD App Registration (client) - client_secret"
+  type        = string
+  sensitive   = true
+  
+}
 
-variable "api_app_id" {}
+variable "api_app_id" {
+  description = "App Registration ID of the API (Azure Naming Tool)"
+  type        = string
+}
 
-variable "api_url" {}
-
-variable "resource_type" {}
+variable "api_url" {
+  description = "Base URL for Azure Naming Tool API (e.g., https://azurenamingtool-xxxx.azurewebsites.net/)."
+  type        = string
+}
 
 variable "components" {
-  type = map(string)
+  description = "Map of component names and their values."
+  type        = map(string)
 }
--- variables.tf
+++ variables.tf
@@ -8,25 +8,9 @@ variable "api_url" {
   type        = string
 }
 
-variable "resource_type" {
-  description = "Azure resource type (e.g., 'redis')."
-  type        = string
-}
-
 variable "components" {
   description = "Map of component names and their values."
   type        = map(string)
-}
-
-variable "access_token" {
-  description = "OAuth2 Bearer token for Azure AD authentication."
-  type        = string
-  default     = null
-  sensitive   = true
-  /*  validation {
-    condition     = can(regex("^[A-Za-z0-9-_.~+/]+=*$", var.access_token))
-    error_message = "The access_token must be a valid OAuth2 Bearer token."
-  }*/
 }
 
 variable "client_id" {
@@ -48,10 +32,4 @@ variable "client_secret" {
 variable "api_app_id" {
   description = "App Registration ID of the API (Azure Naming Tool)"
   type        = string
-}
-
-variable "existing_generated_name_id" {
-  description = "If provided, use this existing generated name ID for lookup/validation instead of generating a new name."
-  type        = string
-  default     = null
 }
